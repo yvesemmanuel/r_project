976b028f76fbbba1650e21c373a2e1907fe2e3c1
dlondres420 <62063056+dlondres420@users.noreply.github.com>
2021-08-16 11:44:26 -0300

    Add files via upload

--- server.R
+++ server.R
@@ -7,8 +7,21 @@ server <- function(input, output){
         
         dataframe_selecionado <- dados %>% select("date", medida_selecionada) %>% filter(date >= intervalo_tempo[1] & date <= intervalo_tempo[2])
         
-        return (dataframe_selecionado)
+        return(dataframe_selecionado)
     })
+    
+    classes_selecionadas_2 <- eventReactive(input$submeter_comparacao, {
+        
+        medidas_selecionadas <- input$medidas_comparadas
+        intervalo_tempos_2 <- input$intervalo_tempos_2
+
+        
+        dataframe_selecionado_2 <- dados %>% select("date", medidas_selecionadas) %>% filter(date >= intervalo_tempos_2[1] & date <= intervalo_tempos_2[2])
+        
+        return(dataframe_selecionado_2)
+        
+    })
+    
     
     output$data <- renderUI({
         
@@ -29,28 +42,75 @@ server <- function(input, output){
                        language = "pt-BR")
     })
     
+    
+    output$datas_comparacao <- renderUI({
+        
+        medidas_selecionadas <- input$medidas_comparadas
+        
+        df <- dados %>% select("date")
+        
+        tempo_minimo <- min(dados$date)
+        tempo_maximo <- max(dados$date)
+        
+        dateRangeInput("intervalo_tempos_2", "Periodo de analise:",
+                       end = tempo_maximo,
+                       start = tempo_minimo,
+                       min = tempo_minimo,
+                       max = tempo_maximo,
+                       format = "dd/mm/yy",
+                       separator = " - ",
+                       language = "pt-BR")
+    })
+    
     getmode <- function(x) {
         which.max(tabulate(x))
     }
     
+    
     info_datatable <- eventReactive(input$submeter, {
         df <- classe_selecionada()
-        medida_selecionada <- input$medida
-        lista_classe <- pull(df, medida_selecionada)
-
-        media <- mean(lista_classe, na.rm = TRUE)
-        moda <- getmode(lista_classe)
-        mediana <- median(lista_classe, na.rm = TRUE)
-        desvio_padrao <- sd(lista_classe, na.rm = TRUE)
-        valor_maximo <- max(lista_classe, na.rm = TRUE)
-        valor_minimo <- min(lista_classe, na.rm = TRUE)
-
-        df_tb <- data.frame(medida_selecionada, media, moda, mediana, desvio_padrao, valor_maximo, valor_minimo)
+        Medida <- input$medida
+        
+        lista_classe <- pull(df, Medida)
+        
+        Media <- mean(lista_classe, na.rm = TRUE)
+        Moda <- getmode(lista_classe)
+        Mediana <- median(lista_classe, na.rm = TRUE)
+        DesvioPadrao <- sd(lista_classe, na.rm = TRUE)
+        ValorMaximo <- max(lista_classe, na.rm = TRUE)
+        ValorMinimo <- min(lista_classe, na.rm = TRUE)
+        
+        df_tb <- data.frame(Medida, Media, Moda, Mediana, DesvioPadrao, ValorMaximo, ValorMinimo)
+        
         df_tb <- as.data.frame(t(df_tb))
-
-        return (df_tb)
-
+        
+        return(df_tb)
+        
     })
+    
+    info_datatable_corr <- eventReactive(input$submeter_comparacao, {
+        df_2 <- classes_selecionadas_2()
+        Medida_2 <- input$medidas_comparadas
+        
+        lista_classe_2 <- pull(df_2, Medida_2[1])
+        lista_classe_3 <- pull(df_2, Medida_2[2])
+        
+        correlation <- cor(lista_classe_3, lista_classe_2, method = "pearson", use = "complete.obs")
+        return(correlation)
+    })
+    
+
+    output$corr_table <- renderDT({
+        info_datatable_corr() %>%
+            as.data.frame() %>%
+            DT::datatable(options = list(
+                language = list(
+                    url = "//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json"
+                )
+            ))
+        
+    })
+    
     
     output$info <- renderDT({
         info_datatable() %>%
@@ -60,12 +120,14 @@ server <- function(input, output){
                     url = "//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json"
                 )
             ))
+        
     })
     
-    output$grafico_linha <- renderPlot({
+    output$sh <- renderPlot({
         df <- classe_selecionada()
-        medida_selecionada <- input$medida
-        lista_classe <- pull(df, medida_selecionada)
+        Medida <- input$medida
+        
+        lista_classe <- pull(df, Medida)
         
         valor_minimo <- min(lista_classe)
         valor_maximo <- max(lista_classe)
@@ -74,6 +136,7 @@ server <- function(input, output){
         grafico <- df %>%
             ggplot(aes(date, lista_classe, group = 1)) +
             geom_line() +
+            ylab(Medida) +
             coord_cartesian(ylim = c(valor_minimo, valor_maximo)) +
             theme_bw() +
             scale_x_date(date_labels = "%Y-%m-%d")
@@ -81,16 +144,16 @@ server <- function(input, output){
         grafico
     })
     
-    
     output$histograma <- renderPlot({
         df <- classe_selecionada()
         medida_selecionada <- input$medida
         
-        frequencias <- pull(df, medida_selecionada)
-
+        frequências <- pull(df, medida_selecionada)
+        
         grafico <- df %>%
-            ggplot(aes(frequencias)) +
+            ggplot(aes(frequências)) +
             geom_histogram() +
+            ylab(medida_selecionada) +
             theme_bw()
         
         grafico
@@ -100,16 +163,77 @@ server <- function(input, output){
         df <- classe_selecionada()
         medida_selecionada <- input$medida
         
-        valores <- pull(df, medida_selecionada)
+        valor <- pull(df, medida_selecionada)
         
         grafico <- df %>%
-            ggplot(aes(valores)) +
+            ggplot(aes(valor)) +
             geom_boxplot() +
             theme_bw()
         
         grafico
     })
     
+    output$scatter <- renderPlot({
+        df <- classes_selecionadas_2()
+        medidas_selecionadas <- input$medidas_comparadas
+        
+        valores_2 <- pull(df, medidas_selecionadas[1])
+        valores_3 <- pull(df, medidas_selecionadas[2])
+
+        
+        grafico <- df %>%
+            ggplot(aes(x = valores_2, y = valores_3)) +
+            xlab(medidas_selecionadas[1]) +
+            ylab(medidas_selecionadas[2]) +
+            geom_point() +
+            theme_bw()
+        
+        grafico
+        
+    })
+    
+    output$series_graph <- renderPlot({
+        df <- classes_selecionadas_2()
+        medidas_selecionadas <- input$medidas_comparadas
+        
+        classe_1 <- pull(df, medidas_selecionadas[1])
+        classe_2 <- pull(df, medidas_selecionadas[2])
+        
+        df$date <- ymd(df$date)
+        
+        grafico <- df %>%
+            ggplot(aes(date)) +
+            geom_line(aes(y = classe_1), color = "blue") +
+            geom_line(aes(y = classe_2), color = "black") 
+
+            
+        
+        grafico
+        
+
+        
+    })
+    
+    output$averages_graph <- renderPlot({
+        df <- classes_selecionadas_2()
+        medidas_selecionadas <- input$medidas_comparadas
+        
+        dat <- data.frame(
+            Medias = c(mean(pull(df, medidas_selecionadas[1])),
+                       mean(pull(df, medidas_selecionadas[2]))),
+            Medidas = as.factor(c(medidas_selecionadas[1], medidas_selecionadas[2]))
+        )
+        
+        dat_long <- dat %>%
+            gather("Medida", "Valor", -Medidas)
+        
+        grafico <- dat_long %>%
+            ggplot(aes(x=Medidas, y = Valor)) +
+            geom_col(position = "dodge")
+        
+        
+        grafico
+    })
     
     
-    }
+}
